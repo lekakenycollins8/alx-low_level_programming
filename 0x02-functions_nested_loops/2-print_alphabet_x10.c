f93b73b1967b1798cc99f1ce7597661db265eeda
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-08-15 09:35:12 -0700

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Prints 10 times the alphabet
+ * Return: 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+
+	for (int i = 0; i < 10; i++)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+	}
+}
