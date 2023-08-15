3afba8dc659af27c4acde3b3e8953742272af662
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-08-15 15:00:16 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,18 @@
 
 /**
  * print_last_digit - function
- * @r: character value of integer
+ * @num: character value of integer
  * Fucntion that prints last digit of a number
  * Return: Last digit
  */
-int print_last_digit(int r)
+int print_last_digit(int num)
 {
-	int last_digit = r % 10;
+	int last_digit = num % 10;
+
+	if (last_digit < 0)
+	{
+		last_digit *= -1;
+	}
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
