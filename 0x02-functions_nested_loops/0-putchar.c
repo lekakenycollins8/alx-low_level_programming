2a2ccb3deca2eb0c821c6113b89f7ae52cf4f41e
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-08-15 08:15:34 -0700

    Header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Program that prints _putchar
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	putchar('_putchar');
+	putchar('\n');
+	return (0);
+}
