ba108e20b0fee3ba006df43ff0b64ebaed5659c3
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-08-15 09:02:39 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Prints alphabet in lowercase
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+	int l = 97;
+
+	while (l <= 122)
+	{
+		_putchar(l);
+		l++;
+	}
+	_putchar('\n');
+}
