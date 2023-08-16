508bc133dcbe327c08029ebd3547c07b154c5214
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-08-16 01:59:46 -0700

    addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,12 +2,15 @@
 
 /**
  * add - Function
+ * @num1: first integer
+ * @num2: second integer
  * Function that adds two integers
  * Return: result
  */
 int add(int num1, int num2)
 {
 	int result;
+
 	result = num1 + num2;
 	return (result);
 }
