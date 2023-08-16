ccd8e4852b4bebf29064b164e904434ff6cf22cc
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-08-16 01:51:45 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Function
+ * Function that adds two integers
+ * Return: result
+ */
+int add(int num1, int num2)
+{
+	int result;
+	result = num1 + num2;
+	return (result);
+}
