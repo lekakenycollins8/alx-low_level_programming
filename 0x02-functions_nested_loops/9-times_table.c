f05371c002efd58a9ef96069c004763e8d43154e
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-08-16 01:34:27 -0700

    9-times-table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,21 +14,25 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			int product = i * j;
-
-			if (product < 10)
+			
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (product < 10 && j != 0)
 			{
 				_putchar(' ');
 				_putchar(product + '0');
 			}
-			else
+			else if (product >= 10)
 			{
 				_putchar(product / 10 + '0');
 				_putchar(product % 10 + '0');
 			}
-			if (j != 9)
+			else
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar(product + '0');
 			}
 		}
 		_putchar('\n');
