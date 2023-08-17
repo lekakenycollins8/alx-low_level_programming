2c8cc6a7024f7049cc44922f495486f7b8dfe20d
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-08-17 16:59:25 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - Entry point
+ * @size: size of the square
+ * Prints a square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+		int j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar ('\n');
+		}
+	}
+}
