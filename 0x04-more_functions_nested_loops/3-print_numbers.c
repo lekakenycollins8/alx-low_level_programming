0c18d34f032d1f8c9ddcf64ad1f9bd64d1358508
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-08-17 14:17:02 -0700

    print numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ * Prints numbers from 0 to 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
