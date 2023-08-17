1b7360b77afde402d44d492320b9fc6ea2880b54
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-08-17 16:17:01 -0700

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * Draws a straight line in the terminal
+ * @n: numbers of times character _ should be printed
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
