0390c688d7471f5dc40572beb8c0a913ca4fabac
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-08-21 15:25:54 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcpy - Entry point
+ * Copies string pointed to by src, including the null byte
+ * to the buffer pointed to by dest
+ * @dest: Pointer to destination buffer
+ * @src: Pointer to source string
+ * Return: Pointer to destination buffer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	
+	return (dest);
+}
