01b705bcb095aded2be921baaa68cdc9ceba0b61
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-08-21 12:03:04 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * Takes an int as parameter and updates the value it points to 98
+ * @n: character integer takes
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
