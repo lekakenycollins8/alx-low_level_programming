538090880fb3e4df0f39e1524c3abf56397b7a7f
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-08-21 14:55:38 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - Entry point
+ * Prints half of a string
+ * @str: pointer to string
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int start_index;
+	
+	while (str[length != '\0'])
+	{
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		start_index = length / 2;
+	}
+	else
+	{
+		start_index = (length - 1) / 2;
+	}
+	while (start_index < length)
+	{
+		_putchar(str[start_index]);
+		start_index++;
+	}
+	_putchar('\n');
+}
+
