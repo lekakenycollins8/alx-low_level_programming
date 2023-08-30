7cf981230207e409d10ef69062175c8302330f54
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-08-30 11:14:56 -0700

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns length of a string
+ * @s: Pointer to string
+ * Return: length
+ */
+
+int _strlen_recursion(char *s)
+{
+	int length;
+
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	length = _strlen_recursion (s + 1);
+
+	return (length + 1);
+}
