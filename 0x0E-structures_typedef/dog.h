1cc4608244da2200bb8fb4c43a8aad6c9988720c
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-09-11 12:46:16 -0700

    dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -26,7 +26,7 @@ typedef struct dog_t
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
