4e69b50b88a01e4b2ee1f7ca248de17821232f1d
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-09-14 13:36:25 -0700

    correction print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,79 +1,98 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
- * print_all - Prints anything
- * @format: list of types passed to argument
+ * print_char - print a character
+ * args: va_list with the character to print as its next element
+ * Return: number of bytes printed
  */
 
+int print_char(va_list args)
+{
+	return (printf("%c", va_arg(args, int)));
+}
+
+/**
+ * print_float - Print a float
+ * @args: va_list with the float to print as its next element
+ * Return: the number of bytes printed
+ */
+
+int print_float(va_list args)
+{
+	return (printf("%f", va_arg(args, double)));
+}
+
+/**
+ * print_int - Print an integer
+ * @args: va_list with the integer to print as its next element
+ * Return: the number of bytes printed
+ */
+
+int print_int(va_list args)
+{
+	return (printf("%i", va_arg(args, int)));
+}
+
+/**
+ * print_str - print a string
+ * @args: the va_list with the string to print as it's next element
+ *
+ * Return: the number of bytes printed
+ */
+int print_str(va_list args)
+{
+	const char *str = va_arg(args, const char *);
+
+	if (!str)
+	{
+		str = "(nil)";
+	}
+	return (printf("%s", str));
+}
+
+/**
+ * print_all - print anything
+ * @format: a format string listing the types of the proceeding arguments
+ * @...: the values to print
+ */
 void print_all(const char * const format, ...)
 {
-	char char_arg;
-	int int_arg;
-	double double_arg;
-	char *string_arg;
 	const char *current = format;
 	int first_item = 1;
 
 	va_list args;
+
 	va_start(args, format);
 
 	while (*current)
 	{
-		if (*current == 'c')
+		if (!first_item)
 		{
-			if (!first_item)
-			{
-				printf(", ");
-			}
-			char_arg = va_arg(args, int);
-
-			printf("%c", char_arg);
+			printf(", ");
 		}
-		else if (*current == 'i')
+		switch (*current)
 		{
-			if (!first_item)
-			{
-				printf(", ");
-			}
-			int_arg = va_arg(args, int);
-
-			printf("%d", int_arg);
-		}
-		else if (*current == 'f')
-		{
-			if (!first_item)
-			{
-				printf(", ");
-			}
-			double_arg = va_arg(args, double);
-
-			printf("%f", double_arg);
-		}
-		else if (*current == 's')
-		{
-			if (!first_item)
-			{
-				printf(", ");
-			}
-			string_arg = va_arg(args, char *);
-
-			if (string_arg != NULL)
-			{
-				printf("%s", string_arg);
-			}
-			else
-			{
-				printf("(nil)");
-			}
-		}
-		else
-		{
+			case 'c':
+				print_char(args);
+				break;
+			case 'f':
+				print_float(args);
+				break;
+			case 'i':
+				print_int(args);
+				break;
+			case 's':
+				print_str(args);
+				break;
+			default:
+			break;
 		}
 		first_item = 0;
-		current++;
+		current ++;
 	}
-	printf("\n");
 	va_end(args);
+	printf("\n");
 }
+
