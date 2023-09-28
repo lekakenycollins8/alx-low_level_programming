63621065ccefc10e3bc7e19f1685106a98723693
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-09-28 11:01:35 -0700

    correction

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,21 +7,26 @@
 
 void print_binary(unsigned long int n)
 {
-	int print = 0;
-	int Number_of_bits = sizeof(n) * 8;
-	int mask = 1 << (Number_of_bits - 1);
+	int bit_position = sizeof(n) * 8 - 1;
+	unsigned long int mask = 1UL << bit_position;
+	int flag = 0;
 
 	while (mask > 0)
 	{
 		if (n & mask)
 		{
 			_putchar('1');
-			print = 1;
+			flag = 1;
 		}
-		else if (print)
+		else
 		{
 			_putchar('0');
 		}
 		mask >>= 1;
+		bit_position--;
+	}
+	if (!flag)
+	{
+		_putchar('0');
 	}
 }
