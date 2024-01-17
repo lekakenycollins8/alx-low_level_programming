7dc3cd6638039b671892e9b338bd048f14b11a73
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-01-17 02:13:04 -0800

    correction

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,19 +35,10 @@ typedef struct hash_table_s
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
-void shash_table_print(const shash_table_t *ht);
-shash_table_t *shash_table_create(unsigned long int size);
-int sort_n_insert(shash_table_t *ht, shash_node_t *item);
-int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-int screate_item(shash_table_t *ht, const char *key,
-		const char *value, unsigned long int idx);
-void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht);
-char *shash_table_get(const shash_table_t *ht, const char *key);
 #endif /* HASH_TABLES_H */
